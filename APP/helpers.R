5aad02c7b426c14a301eaae4579151121f7304c8
ajchenwenjie <30453500+ajchenwenjie@users.noreply.github.com>
2025-08-16 17:19:27 -0500

    Update helpers.R

--- APP/helpers.R
+++ APP/helpers.R
@@ -17,7 +17,7 @@ required_packages <- c(
   "broom", "shiny", "ggplot2", "dplyr", "tidyr", "data.table", 
   "stringr", "scales", "gridExtra", "ggsci", "patchwork", 
   "ggdist", "ggthemes", "ggrepel", "plotly", "textshape", 
-  "survival", "survminer", "purrr", "shinycssloaders", "grid", "fst"
+  "survival", "survminer", "shinycssloaders", "grid", "fst"
 )
 
 # Load packages with error handling
